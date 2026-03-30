d46ac6adad31dbed61b9342e51cdb5893f169197
Pranav Maguluru <pranavmag123@gmail.com>
2026-03-30 00:49:26 -0400

    Wrote a Memory struct with byte-addressed RAM, read and write functions for byte, halfword, and word sizes, and correct little-endian byte ordering. Wrote cases for all Branch instructions and a couple Load memory instructions.

--- interpreter.cpp
+++ interpreter.cpp
@@ -15,7 +15,39 @@ uint32_t Core::readReg(int regNum) {
 	return registers[regNum];
 }
 
-void Core::execute(const DecodedInstruction& inst) {
+uint32_t Memory::readByte(uint32_t addr) {
+	return memory[addr];
+}
+
+uint32_t Memory::readHalfWord(uint32_t addr) {
+	return memory[addr] |
+		(memory[addr + 1] << 8);
+}
+
+uint32_t Memory::readWord(uint32_t addr) {
+	return  memory[addr] |
+		(memory[addr + 1] << 8) |
+		(memory[addr + 2] << 16) |
+		(memory[addr + 3] << 24);
+}
+
+void Memory::writeByte(uint32_t addr, uint32_t value) {
+	memory[addr] = value & 0xFF;
+}
+
+void Memory::writeHalfWord(uint32_t addr, uint32_t value) {
+	memory[addr] = value & 0xFF;
+	memory[addr + 1] = (value >> 8) & 0xFF;
+}
+
+void Memory::writeWord(uint32_t addr, uint32_t value) {
+	memory[addr] = value & 0xFF;
+	memory[addr + 1] = (value >> 8) & 0xFF;
+	memory[addr + 2] = (value >> 16) & 0xFF;
+	memory[addr + 3] = (value >> 24) & 0xFF;
+}
+
+void Core::execute(const DecodedInstruction& inst, Memory& mem) {
 	switch (inst.name) {
 	case Instruction::ADDI: {
 		writeReg(inst.rd, readReg(inst.rs1) + inst.imm);
@@ -97,7 +129,102 @@ void Core::execute(const DecodedInstruction& inst) {
 		writeReg(inst.rd, result);
 		break;
 	}
+	case Instruction::BEQ: {
+		if (readReg(inst.rs1) == readReg(inst.rs2)) {
+			pc = pc + inst.imm;
+		}
+		else {
+			pc = pc + 4;
+		}
+		break;
+	}
+	case Instruction::BNE: {
+		if (readReg(inst.rs1) != readReg(inst.rs2)) {
+			pc = pc + inst.imm;
+		}
+		else {
+			pc = pc + 4;
+		}
+		break;
+	}
+	case Instruction::BLT: {
+		if (static_cast<int32_t>(readReg(inst.rs1)) < static_cast<int32_t>(readReg(inst.rs2))) {
+			pc = pc + inst.imm;
+		}
+		else {
+			pc = pc + 4;
+		}
+		break;
+	}
+	case Instruction::BLTU: {
+		if (readReg(inst.rs1) < readReg(inst.rs2)) {
+			pc = pc + inst.imm;
+		}
+		else {
+			pc = pc + 4;
+		}
+		break;
+	}
+	case Instruction::BGE: {
+		if (static_cast<int32_t>(readReg(inst.rs1)) >= static_cast<int32_t>(readReg(inst.rs2))) {
+			pc = pc + inst.imm;
+		}
+		else {
+			pc = pc + 4;
+		}
+		break;
+	}
+	case Instruction::BGEU: {
+		if (readReg(inst.rs1) >= readReg(inst.rs2)) {
+			pc = pc + inst.imm;
+		}
+		else {
+			pc = pc + 4;
+		}
+		break;
+	}
+	case Instruction::JAL: {
+		writeReg(inst.rd, pc + 4);
+		pc = pc + inst.imm;
+		break;
+	}
+	case Instruction::JALR: {
+		writeReg(inst.rd, pc + 4);
+		pc = (readReg(inst.rs1) + inst.imm) & 0xFFFFFFFE;
+		break;
+	}
+	case Instruction::LB: {
+		uint32_t memory = (mem.readByte(readReg(inst.rs1) + inst.imm));
+		if (memory & 0x80) {
+			memory = memory | 0xFFFFFF00;
+		}
+		writeReg(inst.rd, memory);
+		break;
+	}
+	case Instruction::LBU: {
+		uint32_t memory = mem.readByte(readReg(inst.rs1) + inst.imm);
+		writeReg(inst.rd, memory);
+		break;
+	}
 
+	case Instruction::LH: {
+		uint32_t memory = mem.readHalfWord(readReg(inst.rs1) + inst.imm);
+		if (memory & 0x8000) {
+			memory = memory | 0xFFFF0000;
+		}
+		writeReg(inst.rd, memory);
+		break;
+	}
+	case Instruction::LHU: {
+		uint32_t memory = mem.readHalfWord(readReg(inst.rs1) + inst.imm);
+		writeReg(inst.rd, memory);
+		break;
+	}
+	case Instruction::LW: {
+		uint32_t memory = mem.readWord(readReg(inst.rs1) + inst.imm);
+		writeReg(inst.rd, memory);
+		break;
+	}
 
 
 	}
--- interpreter.h
+++ interpreter.h
@@ -6,6 +6,22 @@
 #include <iostream>
 #include <cstdint>
 
+struct Memory {
+	std::array<uint8_t, 65536> memory;
+
+	uint32_t readByte(uint32_t addr);
+
+	uint32_t readHalfWord(uint32_t addr);
+
+	uint32_t readWord(uint32_t addr);
+
+	void writeByte(uint32_t addr, uint32_t value);
+
+	void writeHalfWord(uint32_t addr, uint32_t value);
+
+	void writeWord(uint32_t addr, uint32_t value);
+};
+
 struct Core {
 	std::array<uint32_t, 32> registers;
 	uint32_t pc;
@@ -15,5 +31,5 @@ struct Core {
 
 	uint32_t readReg(int regNum);
 
-	void execute(const DecodedInstruction& inst);
+	void execute(const DecodedInstruction& inst, Memory& mem);
 };
