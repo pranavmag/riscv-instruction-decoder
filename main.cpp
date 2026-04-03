eedabc6d7a9d366e8423570eaa2e63bfcbb9d8dc
Pranav Maguluru <pranavmag123@gmail.com>
2026-04-02 23:43:02 -0400

    Hardcoded a fibonacci program to test my knowledge for that. Although will remove in future iterations because it is redundant to have it for .bin or other implementations for now.

--- main.cpp
+++ main.cpp
@@ -17,6 +17,7 @@ int main() {
 	std::cout << "1 - Disassembler\n";
 	std::cout << "2 - Interpreter (manual-input mode)\n";
 	std::cout << "3 - Interpreter (load .bin file)\n";
+	std::cout << "4 - Fibonacci Program\n";
 
 	int choice{};
 	std::cin >> choice;
@@ -74,7 +75,7 @@ int main() {
 		Memory mem;
 		Core core;
 
-		int addr{};
+		uint32_t addr{};
 
 		while (true) {
 
@@ -111,6 +112,35 @@ int main() {
 		std::getline(std::cin >> std::ws, fileName);
 
 		mem.loadProgram(fileName);
+
+		core.run(mem);
+
+		for (int i{}; i < 32; i++) {
+			std::cout << "x" << i << " = " << core.readReg(i) << '\n';
+		}
+	}
+
+	if (choice == 4) {
+		Memory mem;
+		Core core;
+
+		std::vector<uint32_t> fibProgram = {
+			0x00000093,
+			0x00100113,
+			0x00A00193,
+			0x00208233,
+			0x002000B3,
+			0x00400133,
+			0xFFF18193,
+			0xFE0198E3,
+			0x00100073
+		};
+
+		uint32_t addr{};
+		for (uint32_t inst : fibProgram) {
+			mem.writeWord(addr, inst);
+			addr += 4;
+		}
 
 		core.run(mem);
 
